210056856414dddd70ad459a72da1f0c70ebd3ab
your Name Here <yee0230@naver.com>
2016-12-07 09:51:31 +0900

    현재 진행... 진행중....
    player를 만들었지만 Deck에서 카드를 나누어 주지 못함

--- main.cpp
+++ main.cpp
@@ -3,11 +3,13 @@
 #include <assert.h>
 #include <stdio.h>
 #include <string.h>
-#include <stack>
 #include <cstdlib>
+#include <conio.h>
 #include <ctime>
+#include <time.h>
 #define CMAX 52
 #define TMAX 50
+#define random(num) ((rand()*rand())%num)
 
 using namespace std;
 
@@ -23,7 +25,7 @@ struct SCard { //카드를 구성하는 구조체
 	string getName() {
 			return string(this->Name); //출력을 위한 도구
 	}
-	
+
 //카드객체의 알파벳이 들어가 알파벳에 값을 넣어주는 함수  
 	int GetNumber() const {
 		if (isdigit(Name[0])) return Name[0]-'0';
@@ -42,7 +44,7 @@ struct SCard { //카드를 구성하는 구조체
 	friend ostream &operator <<(ostream &c, const SCard &C) {
 		return c << C.Name;
 	}
-//클래스 객체끼리 크기 비교해 주는 기준을 생성  
+//클래스 객체끼리 크기 비교해 주는 기준을 생성 
 	bool operator ==(const SCard &Other) const {
 		return (strcmp(Name, Other.Name)==0);
 	}
@@ -54,7 +56,7 @@ struct SCard { //카드를 구성하는 구조체
 	}
 	bool operator +(const SCard &Other) const {
 		if (GetNumber() + Other.GetNumber()) return true;
-	}	
+	}
 };
 
 SCard GCard[MaxCard]={
@@ -62,15 +64,6 @@ SCard GCard[MaxCard]={
 	"1♣", "2♣", "3♣", "4♣", "5♣", "6♣", "7♣", "8♣", "9♣", "10♣", "J♣", "Q♣", "K♣",
 	"1♥", "2♥", "3♥", "4♥", "5♥", "6♥", "7♥", "8♥", "9♥", "10♥", "J♥", "Q♥", "K♥", 
 	"1◆", "2◆", "3◆", "4◆", "5◆", "6◆", "7◆", "8◆", "9◆", "10◆", "J◆", "Q◆", "K◆"
-};
-//카드패 스택 
-class DStack {
-	char stack[CMAX];
-	int tos;
-public :
-	void init();
-	void push(char *ch);
-	char pop();
 };
 
 class CCardSet {
@@ -81,7 +74,6 @@ class CCardSet {
 		const int sx,sy;
 		CCardSet(int asx,int asy) : sx(asx), sy(asy) { Num=0;}
 	public:
-		
 // 카드를 하나도 없는 상태로 집합을 생성  
 		int GetNum() { return Num;} 
 		SCard GetCard(int idx) { return Card[idx];}
@@ -97,7 +89,6 @@ class CCardSet {
 //카드를 정렬 
 void CCardSet::insertCard(SCard C) {
 	int i;
-
 	for (i=0;i<Num;i++) {
 		if (C < Card[i]) break;
 	}
@@ -105,7 +96,7 @@ void CCardSet::insertCard(SCard C) {
 	Card[i]=C;
 	Num++;
 }
-//플레이어가 카드를 내거나 데크에서 한장을 나누어줄때 쓸 함수 
+//플레이어가 카드를 내거나 데크에서 한장을 나누어줄때 쓸 함수 정의 
 SCard CCardSet::RemoveCard(int idx) {
 	assert(idx < Num);
 	SCard C=Card[idx];
@@ -113,42 +104,61 @@ SCard CCardSet::RemoveCard(int idx) {
 	Num--;
 	return C;
 }
-//gotoxy함수 
+//gotoxy함수
 void gotoxy(int sx, int sy) {
 COORD Pos = {sx - 1, sy - 1};
 SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
 } 
-//데크 
+//delay함수
+void delay(clock_t sleep)
+{	
+    clock_t cur = clock(), el;
+
+    for(;;){              /* 무한루프를 돌린다. */ 
+        el = clock();  /* 현재까지 프로그램이 실행된 TICK */
+        if((el - cur) > sleep)
+            break	;
+    }
+}
+//randomize 함수 
+void randomize(void) {
+  srand((unsigned) time(NULL));
+  // 첫부분의 숫자는 거의 랜덤하지 않기에, 앞부분에서 랜덤한 개수로 뽑아서 버림
+  for (int i = 0; i < (rand() % RAND_MAX); i++) (rand() % RAND_MAX);
+}
+//데크
 class CDeck : public CCardSet {
 	public:
 	CDeck(int asx,int asy) : CCardSet(asx,asy) { ; }
-
 	    void Shuffle() {
 	        int i,n;
-	        srand((unsigned int)time(NULL));
+	       // srand((unsigned int)time(NULL));
 	    	for (i=0;i<MaxCard;i++) {
 	           	do {
-	               	n=rand() % 52;
+	               	n=random(MaxCard);
 	       		} while (Card[n].Name[0] != NULL);
 	        Card[n]=GCard[i];
 	        Num++;
 		   	}
 	    }
 //한장씩 배분 
-    SCard Pop() { return RemoveCard(Num-1); }
+    SCard Pop() { return RemoveCard(Num-1);}
     bool IsEmpty() { return Num==0; }
     bool IsNotLast() { return Num > 1; }
-//카드남은 데크 두는 곳  
-    void Draw(){
+//카드남은 데크 두는 곳
+    void Draw(bool bFlip){
 		gotoxy(sx,sy);
         cout << "|???| ";
     }
 };
+ 
+/////////////////////////////////////////////여기여기여기; ㅕ아ㅓㄴ오ㅓ라ㅓ;모댜ㅓㅗ휸머ㅏㅇ류ㅣㅂ저ㅏㅗㅠㄷㄹ 
 /*
 class CCopy : public CDeck {
-		public :
+//		SCard.Name;
+		public:
 		void setname(string name) {
-			this->name = name; 
+			this->name = cDeck->GetCard(i).getName();
 		}
 };
 */
@@ -168,25 +178,118 @@ public:
 		}
 	}
 };
+//담요 
+class CBlanket: public CPlayer
+{
+public:
+     CBlanket(int asx,int asy) : CPlayer(asx,asy) { ; }
+     void Draw() {
+          CPlayer::Draw(false);
+     }
+     void DrawSelNum(int *pSame) {
+          int n;
+          int *p;
+          for (n=1,p=pSame;*p!=-1;p++,n++) {
+              gotoxy(sx+*p*CardGap,sy-1);
+              cout << '[' << n << ']';
+          }
+     }
+     void DrawTempCard(int idx,SCard C) {
+          gotoxy(sx+idx*CardGap,sy+1);
+          cout << C;
+     }
+};
+
+CDeck Deck(18,9);
+CPlayer Down(20,5), Up(3,5);
+CBlanket Blanket(5,12);
+//CPlayerPae DownPae(40,10), UpPae(40,10);
+bool DownTurn;
+
+void Initialize() {
+     int i;
+     Deck.Shuffle();
+     for (i=0;i<8;i++) {
+         Down.insertCard(Deck.Pop());
+         Up.insertCard(Deck.Pop());
+         if (i < 4) Blanket.insertCard(Deck.Pop());
+	 }
+};
+
+void DrawScreen() {
+     system("cls");
+     Down.Draw(DownTurn);
+     Up.Draw(!DownTurn);
+     Blanket.Draw();
+     Deck.Draw(false);
+//     UpPae.Draw();
+//     DownPae.Draw();
+}
+
+void OutPrompt(const char *Mes,int Wait/*=0*/) {
+     gotoxy(5,23);
+     for (int i=5;i<79;i++) { cout << ' '; }
+     gotoxy(5,23);
+     cout << Mes;
+     delay(Wait);
+}
+ 
+int InputInt(const char *Mes, int start, int end) {
+     int ch;
+ 
+     OutPrompt(Mes,1);
+     for (;;) {
+          ch=tolower(getch());
+          if (ch == 0xE0 || ch == 0) {
+              ch=getch();
+              continue;
+          }
+          if (!(isdigit(ch) || ch=='a')) continue;
+          if (ch=='a') ch=10; else ch=ch-'0';
+          if (ch >= start && ch <= end) {
+              return ch;
+          }
+          OutPrompt("무효한 번호입니다. 지정한 범위에 맞게 다시 입력해 주세요.",1);
+     }
+}
 
 int main() {
-	
-	
-
-// 카드 객체 출력 	 
-	for(int i=0; i<52; i++) {
-		cout << GCard[rand()%52];
-		cout << " ";
-		if ((i+1) % 13 == 0) {
-			cout <<"\n";
-		};
-	}
-
+	int i,ch;
+     int arSame[4],SameNum;
+     char Mes[256];
+     CPlayer *Turn;
+//     CPlayerPae *TurnPae,*OtherPae;
+     int UserIdx,UserSel,DeckSel;
+     SCard UserCard, DeckCard;
+     bool UserTriple, DeckTriple;
+     int nSnatch;
+     int NewScore;
+     
+     randomize();
+	Initialize();
+	DrawScreen();
+          if (DownTurn) {
+              Turn=&Down;
+//              TurnPae=&SouthPae;
+//            OtherPae=&NorthPae;
+          } else {
+              Turn=&Up;
+//              TurnPae=&NorthPae;
+//              OtherPae=&SouthPae;
+          }
+ 
+          sprintf(Mes,"내고 싶은 카드를 선택하세요(1~%d,0:종료) ",Turn->GetNum());
+          ch=InputInt(Mes,0,Turn->GetNum());
+          if (ch == 0) {
+              if (InputInt("정말 끝낼겁니까?(0:예,1:아니오)",0,1)==0) ;
+                //   return ;
+             // else
+                //   continue;
+          }
 //카드크기 비교  
 	int a = 0; //카드 1 
 	int b = 9; //카드 10
 	int c = 10; //카드 j = 11
-
 	cout << "스페이드 1과 스페이드 10 비교"	;
 	if(GCard[a] < GCard[b]) {  
 		cout << GCard[b];
@@ -200,35 +303,33 @@ int main() {
 		cout << GCard[a];
 	}
 
-//카드패를 놓아두는 곳    
-	CDeck *Deck = new CDeck(20,20);
+//카드패를 놓아두는 곳
+/*	CDeck *Deck = new CDeck(20, 5);
 	Deck->Shuffle();
-	Deck->Draw();
+//	Deck.Pop() ;
+	Deck->Draw(1);
 //    cout << Deck.Draw)<<endl; 
+*?
 //플레이어
-	CPlayer *Player = new CPlayer(30,20);
-	Player->Draw(12);
+	CPlayer *Player = new CPlayer(20,4);
+	Player->Draw(9);
 //나중에 카드의 값 들을 숫자로 나타내는 부분
-	int t;
+	int t,y;
 	t = atoi(GCard[9].Name);
 	printf("\n%d\n", t);
-
-	CDeck *TDeck = new CDeck(22,22);
-	TDeck->Shuffle();
-	int i;
-	for (int u=0;u<52;u++) {
-		cout << SCard();
-	};
-
 //카드의 둘 뒤치를 선정후 섞어주고 택을 만듬
-	CDeck *cDeck = new CDeck(21, 30);
+	CDeck *cDeck = new CDeck(0,0);
 	cDeck->Shuffle();
 	cout <<"Deck 카드 \n" ;
 	for(int i =0;i<52;i++) {
 	cout << cDeck->GetCard(i).getName();
 	};
+//플래이어
+	CPlayer *GPlayer = new CPlayer(15,30);
+//	Deck->Pop();
+//	CPlayer->Draw();
+
 /*	R *r = new R
-	
 	public :
 		void setname(string name) {
 			this->name = name;
@@ -237,21 +338,6 @@ int main() {
 	R.setName(cDeck->GetCard(0).getName())
 	
 	RemoveCar
-*/	
-//택스텍 출력
-//	DStack ds1;
-//	int k;
-//	ds1.init() ;
-//	char h[];
-//	for(int i=0;i<CMAX;i++) {
-//	char c[2];
-//	c[0] = 1;
-//	ds1.push('12');
-//	*h = GCard[1];
-//	};
-//	for (int i=0;i<52;i++) cout <<ds1.pop() <<endl;
-//	SCard *p;
-//	p = Gcard;
+*/
 	return 0;
-
 };
